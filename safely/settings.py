f43b5316e3a399558e7cb86fafae45e855c7e18d
lukaszkukla <lukaszkukla@gmail.com>
2022-07-25 18:11:05 +0000

    Feat: add post view

--- safely/settings.py
+++ safely/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['safely20.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['safely20.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
