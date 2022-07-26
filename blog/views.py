a9d553693815d528b93d6b37285938cc02f9be52
lukaszkukla <lukaszkukla@gmail.com>
2022-07-26 22:38:56 +0000

    deployment commit

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
+
 
 class PostList(generic.ListView):
     model = Post
@@ -26,7 +28,54 @@ class PostDetail(View):
             {
                 'post': post,
                 'comments': comments,
+                'commented': False, # tell user that the comment is awiting approval
                 'liked': liked,
                 'comment_form': CommentForm()
             },
         )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1) # filter active posts only
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on') # filter approved comments only, sort ascending
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False) # call save method but don't save, assign a post to it first
+            comment.post = post # assign an instance of the post
+            comment.save() # save and commit
+            messages.success(request, "You have posted comment for approval")
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            'post_detail.html',
+            {
+                'post': post,
+                'comments': comments,
+                'commented': True,
+                'liked': liked,
+                'comment_form': CommentForm()
+            },
+        )
+
+
+class PostLike(View):
+
+    def post(self, request, slug):
+        post = get_object_or_404(Post, slug=slug)
+
+        # toggle like button 
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug])) # reload page when you toggle on/off
